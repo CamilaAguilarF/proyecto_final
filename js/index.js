b9fc091e924e8bc712959b5637168ff750586f91
marianelabriamonte <marianbriam@gmail.com>
2024-07-25 18:43:16 -0300

    viento

--- js/index.js
+++ js/index.js
@@ -62,3 +62,40 @@ if(nombreIngresado !== null && passwordIngresada !== null && emailIngresado !== 
 console.log(db)
 console.log(JSON.parse(sessionStorage.getItem('datosDelUsuario')))
 
+
+const apiKey = '5f385b7ee64f5951f9a7c69eb8572cdd';
+const city = 'Caleta Olivia, AR';
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=es`;
+
+    fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error en la solicitud');
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.cod !== 200) {
+            throw new Error(data.message);
+        }
+        const windSpeedMs = data.wind.speed;
+        const windSpeedKmh = windSpeedMs * 3.6;  // Conversión de m/s a km/h
+
+        let message = `${windSpeedKmh.toFixed(2)} km/h.`;
+        if (windSpeedKmh > 100) {
+            message += " Precaución, ALERTA ROJA. Evite salir de casa y asegúrese de asegurar objetos que puedan volar.";
+        } else if (windSpeedKmh > 80) {
+            message += " Precaución, ALERTA AMARILLA. Evite actividades al aire libre y asegúrese de asegurar objetos sueltos.";
+        }
+
+        document.getElementById('wind').textContent = message;
+    })
+    .catch(error => {
+        console.error('Error al obtener los datos:', error);
+        document.getElementById('wind').textContent = 'No se pudo obtener la velocidad del viento';
+    });
+   
+
+
+
+
