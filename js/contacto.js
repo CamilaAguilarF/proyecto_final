194028b2f04c412b3e493c61ad16677615981c05
marianelabriamonte <marianbriam@gmail.com>
2024-07-28 10:53:58 -0300

    contacto

--- js/contacto.js
+++ js/contacto.js
@@ -1,14 +1,56 @@
-let formulario = document.getElementById('form-contacto');
+window.addEventListener('load', function(e){
 
-formulario.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    alert('¡Mensaje enviado! Gracias por tu consulta :)');
+    let usuariosLocalStorage = localStorage.getItem('usuariosContacto');
+    console.log(usuariosLocalStorage);
+ 
+    let usuariosContacto;
+ 
+    if(usuariosLocalStorage === null){
+        usuariosContacto = [];
+    } else {
+        usuariosContacto = JSON.parse(usuariosLocalStorage);
+    }
+
+    let formulario = document.getElementById("form-contacto");
+    let email= document.querySelector('#email');
+    let nombre= document.querySelector('#nombre');
+    let mensaje= document.querySelector('#mensaje');
+    console.log(formulario, email, nombre, mensaje);
+
+    formulario.addEventListener('submit', function(e){
+        e.preventDefault();
+
+        let usuario = {
+            nombre: '',
+            email: '',
+            mensaje: ''
+        };
+
+        if(email.value != ''){
+            usuario.email = email.value;
+        }
+
+        if(nombre.value != ''){
+            usuario.nombre = nombre.value;
+        }
+
+        if(mensaje.value != ''){
+            usuario.mensaje = mensaje.value;
+        }
     
-    const nombre = parseFloat(document.getElementById('nombre').value);
-    const email = parseFloat(document.getElementById('email').value);
-    const mensaje = parseInt(document.getElementById('mensaje').value);
+        console.log(usuario)
+        
+        if(usuario.nombre != ''){
+            usuariosContacto.push(usuario)
+        }
+       console.log(usuariosContacto)
+       localStorage.setItem('usuariosContacto', JSON.stringify(usuariosContacto));
 
-    formulario.reset();
+       email.value = '';
+       nombre.value = '';
+       mensaje.value = '';
 
-   
-});
+       document.getElementById('result').innerText = `Gracias por ponerte en contacto con nosotros, pronto recibirás  una respuesta. `;
+
+    })
+})
